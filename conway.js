1c98e7a834c4a40add6aae3b5aecb50aa1a031ca
Aron Nopanen <aron.nopanen@gmail.com>
2020-02-27 23:49:29 -0800

    Add controls and interactivity
    Add start/stop button, speed slider, and ability to toggle cell state
    on click.

--- conway.js
+++ conway.js
@@ -1,9 +1,15 @@
 const grid = document.getElementById('grid');
-const gridContainer = grid.parentElement;
 // const NUM_COLS = Math.floor(window.innerWidth / 26);
-const NUM_COLS = Math.floor(gridContainer.offsetWidth / 26);
-console.log(NUM_COLS);
-const NUM_ROWS = Math.floor(window.innerHeight / 26);
+// const NUM_ROWS = Math.floor(window.innerHeight / 26);
+
+// const gridContainer = grid.parentElement;
+// const NUM_COLS = Math.floor(gridContainer.offsetWidth / 26);
+// const NUM_ROWS = Math.floor(gridContainer.offsetHeight / 26);
+
+let { width: gridWidth, height: gridHeight } = innerDimensions(grid);
+const CELL_SIZE = 26;
+const NUM_COLS = Math.floor(gridWidth / CELL_SIZE);
+const NUM_ROWS = Math.floor(gridHeight / CELL_SIZE);
 // const NUM_COLS = 45;
 // const NUM_ROWS = 25;
 const NUM_CELLS = NUM_ROWS * NUM_COLS;
@@ -11,7 +17,8 @@ const NUM_CELLS = NUM_ROWS * NUM_COLS;
 function initGrid() {
     grid.innerHTML = '';
     // grid-template-columns: repeat(40, 25px);
-    grid.style.setProperty("grid-template-columns", "repeat(" + NUM_COLS + ", 26px)");
+    grid.style.setProperty("grid-template-columns",
+        "repeat(" + NUM_COLS + ", " + CELL_SIZE + "px)");
     for (var idx = 0; idx < NUM_CELLS; idx++) {
         const cell = document.createElement("div");
         cell.classList.add("cell");
@@ -86,16 +93,63 @@ function liveNeighbors(model, idx) {
     }, 0);
 }
 
+function innerDimensions(node) {
+    var computedStyle = getComputedStyle(node)
+  
+    let width = node.clientWidth // width with padding
+    let height = node.clientHeight // height with padding
+  
+    height -= parseFloat(computedStyle.paddingTop) + parseFloat(computedStyle.paddingBottom)
+    width -= parseFloat(computedStyle.paddingLeft) + parseFloat(computedStyle.paddingRight)
+    return { height, width }
+}
+
 (function() {
+    var timer = null;
+    
+    let b = document.getElementById("startStopButton");
+    let sp = document.getElementById("speedSlider");
+    let sd = document.getElementById("speedDisplay");
+    let speed = sp.value;
+
+    b.addEventListener("click", function(event) {
+        if (timer == null) {
+            let speed = sp.value;
+            timer = window.setInterval(doGeneration, speed);
+            b.innerText = "Stop";
+        } else {
+            window.clearInterval(timer);
+            timer = null;
+            b.innerText = "Start";
+        }
+    });
+
+    sd.value = sp.value + " ms";
+    sp.addEventListener("input", function(event) {
+        let speed = sp.value;
+        sd.value = speed + " ms";
+        if (timer) {
+            window.clearInterval(timer);
+            timer = window.setInterval(doGeneration, speed);
+        }
+    });
+
     initGrid();
     var model = createRandomModel();
     renderModel(model);
 
-    // model = iterateModel(model);
-    // renderModel(model);
+    let cells = document.getElementsByClassName("cell");
+    for (let i=0, cell; cell = cells[i]; i++) {
+        cell.addEventListener('click', function(event) {
+            model[i] = !model[i];
+            renderModel(model);
+        });
+    }
 
-    window.setInterval(function() {
+    function doGeneration() {
         model = iterateModel(model);
         renderModel(model);
-    }, 500);
+    }
+    
+    timer = window.setInterval(doGeneration, speed);
 })();
